f25a6949d21fc5f18194b1a9f998aa217cb75b61
gounani <sunil.gounani@gmail.com>
2017-08-02 14:01:41 +0530

    Update createbucket.py

--- createbucket.py
+++ createbucket.py
@@ -1,5 +1,5 @@
 import boto3
 
-s3.create_bucket(Bucket='mybucket')
+s3.create_bucket(Buket='mybucket')
 s3.create_bucket(Bucket='mybucket', CreateBucketConfiguration={
     'LocationConstraint': 'us-west-2'})
